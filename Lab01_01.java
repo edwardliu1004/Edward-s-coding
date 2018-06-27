1711d352f6e408deac6b9c60509e6b33e87fb46d
edwardliu1004 <40574449+edwardliu1004@users.noreply.github.com>
2018-06-26 22:25:02 -0700

    this is my ASCII pictures code and edwardliu

---
+++ Lab01_01.java
@@ -0,0 +1,20 @@
+public class Lab01_01{
+	public static void main(String[] args){
+	//Here is the ACSII thingy
+	System.out.println("           _                       _   _ _       ");
+	System.out.println("          | |                     | | | (_)      ");
+	System.out.println("   ___  __| |_      ____ _ _ __ __| | | |_ _   _ ");
+	System.out.println("  / _ \\/ _` \\ \\ /\\ / / _` | '__/ _` | | | | | | |");
+	System.out.println(" |  __/ (_| |\\ V  V / (_| | | | (_| | | | | |_| |");
+	System.out.println("  \\___|\\__,_| \\_/\\_/ \\__,_|_|  \\__,_| |_|_|\\__,_|");
+    System.out.print("                _      \n");
+	System.out.print("               | |     \n");
+	System.out.print("   ___ ___   __| | ___ \n");
+	System.out.print("  / __/ _ \\ / _` |/ _ \\\n");
+	System.out.print(" | (_| (_) | (_| |  __/\n");
+	System.out.print("  \\___\\___/ \\__,_|\\___|\n");
+	
+
+
+	}
+}
