f32f4ade234918719ad2c2be50e1dd7ca4668a63
edwardliu1004 <40574449+edwardliu1004@users.noreply.github.com>
2018-06-27 10:34:08 -0700

    this is the challeng!!!!!!!111kjwaw; f

---
+++ Lesson 1/Hello_World.java
@@ -0,0 +1,7 @@
+public class Hello_World {
+	public static void main (String[] args){
+//Printing the words 'Hello, World!'
+	System.out.print("Where is my new line?\n");
+	System.out.println("Hello, World!");
+	}	
+}
